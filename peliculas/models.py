5cc8975041d7c2cbd27d3bcd648f98b98eadd0fa
Facundo Puig <f.puig@itecriocuarto.org.ar>
2026-06-16 00:29:50 -0300

    Estilos para home y detalle_pelicula

--- peliculas/models.py
+++ peliculas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 
@@ -12,6 +13,14 @@ class Pelicula(models.Model):
         ('ciencia_ficcion', 'Ciencia Ficción'),
     ]
 
+    FORMATOS = [
+        ('2d', '2D'),
+        ('3d', '3D'),
+        ('4d', '4D'),
+        ('imax', 'IMAX'),
+        ('dbox', 'D-BOX'),
+    ]
+
     titulo = models.CharField(max_length=200)
     genero = models.CharField(max_length=50, choices=GENEROS)
     duracion = models.PositiveIntegerField(help_text='Duración en minutos')
@@ -20,6 +29,7 @@ class Pelicula(models.Model):
     trailer_url = models.URLField(blank=True)
     en_cartelera = models.BooleanField(default=True)
     fecha_estreno = models.DateField()
+    formatos = MultiSelectField(choices=FORMATOS, blank=True)
 
     def __str__(self):
         return self.titulo
